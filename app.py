95a62753dfe0a197e25ecfbdbac1c7601cf4e0d7
Chris Rowe <chris.rowe@wri.org>
2023-01-25 20:34:42 -0500

    graph

--- app.py
+++ app.py
@@ -1,22 +1,49 @@
 import panel as pn
 
+# Page setup
 pn.extension(sizing_mode="stretch_width", template="fast")
 
+
+# Header
 pn.state.template.param.update(
-    site_url="https://awesome-panel.org",
-    site="Awesome Panel",
-    title="Hello World",
+    site_url="",
+    site="VSECU Oil Price",
+    title="Data",
 )
 
+
+# Intro
 pn.panel(
     "This is a log of the price of Irving Oil from [VSECU](https://www.vsecu.com/personal/home-heating/fuel-buying-program)"
 ).servable()
 
-import pandas as pd; import numpy as np; import matplotlib.pyplot as plt
+
+# Get data
+import pandas as pd
 
 csv_file = 'https://raw.githubusercontent.com/chrowe/log-heating-oil-price/main/irving_oil_prices.csv'
-data = pd.read_csv(csv_file, parse_dates=['date'], index_col='date')
+data = pd.read_csv(csv_file, parse_dates=['date'])
 
+data.dtypes
+
+# Graph
+from bokeh.io import show
+from bokeh.plotting import figure
+
+p = figure(x_axis_type="datetime", title="Oil Price", height=350, width=800)
+p.xgrid.grid_line_color=None
+p.ygrid.grid_line_alpha=0.5
+p.xaxis.axis_label = 'Day'
+p.yaxis.axis_label = 'Price'
+
+p.line(data.date, data.price)
+
+pn.panel(
+    p
+).servable()
+
+
+# Table
 pn.panel(
     data
 ).servable()
