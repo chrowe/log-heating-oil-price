ce03e94cdca4bf05a460cf8f4f88072c4fe93fe1
Chris Rowe <chris.rowe@wri.org>
2023-02-03 18:16:23 -0500

    graph tooltips

--- index.py
+++ index.py
@@ -20,21 +20,29 @@ pn.panel(
 
 # Get data
 import pandas as pd
+from bokeh.models import ColumnDataSource
 
 csv_file = 'https://raw.githubusercontent.com/chrowe/log-heating-oil-price/main/irving_oil_prices.csv'
-data = pd.read_csv(csv_file, parse_dates=['date'])
+data = ColumnDataSource(pd.read_csv(csv_file, parse_dates=['date']))
 
 
 # Graph
 from bokeh.plotting import figure
 
-p = figure(x_axis_type="datetime", title="Oil Price", height=350, width=800)
+TOOLTIPS = [
+    ("Date", "@date{%F}"),
+    ("Price", "$@price"),
+]
+
+
+p = figure(x_axis_type="datetime", title="Oil Price", height=350, width=800, tooltips=TOOLTIPS)
 p.xgrid.grid_line_color=None
 p.ygrid.grid_line_alpha=0.5
 p.xaxis.axis_label = 'Day'
 p.yaxis.axis_label = 'Price'
+p.hover.formatters = { "@date": "datetime"}
 
-p.line(data.date, data.price)
+p.line(x='date', y='price', source=data)
 
 pn.panel(
     p
