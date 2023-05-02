93171f8bd206f5cceb5fb0891786e874f86be3f4
Chris Rowe <chris.rowe@wri.org>
2023-05-01 22:44:58 -0400

    don't load all of pandas

--- index.py
+++ index.py
@@ -19,11 +19,11 @@ pn.panel(
 
 
 # Get data
-import pandas as pd
+from pandas import read_csv
 from bokeh.models import ColumnDataSource
 
 csv_file = 'https://raw.githubusercontent.com/chrowe/log-heating-oil-price/main/irving_oil_prices.csv'
-data = pd.read_csv(csv_file, parse_dates=['date']).sort_values(by=['date'], ascending=False)
+data = read_csv(csv_file, parse_dates=['date']).sort_values(by=['date'], ascending=False)
 
 
 # Graph
